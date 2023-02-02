ccff65824daeb9f35e44dcc86071c396adc02a01
Day Little <dayana.y.little@gmail.com>
2023-02-01 22:03:31 -0500

    create routes for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Projects from './pages/Projects/Projects';
+import Resume from './pages/Resume/Resume';
+
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+   <Routes>
+   <Route path='about' element={<About />} />
+   <Route path='/' element={<Home />} />
+   <Route path='contact' element={<Contact />} />
+   <Route path='resume' element={<Resume />} />
+   <Route path='projects' element={<Projects />} />
+
+   </Routes>
+   </>
+  )
 }
 
 export default App;
