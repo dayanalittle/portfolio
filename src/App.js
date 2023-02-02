a0a6cbb16df98345c51e006001576cd6400db364
Day Little <dayana.y.little@gmail.com>
2023-02-01 22:24:25 -0500

    create Nav and import Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+
+
+// Components
+import NavBar from './components/Nav';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 import Projects from './pages/Projects/Projects';
 import Resume from './pages/Resume/Resume';
+
 
 import logo from './logo.svg';
 import './App.css';
@@ -12,6 +17,7 @@ import './App.css';
 function App() {
   return (
     <>
+    <NavBar />
    <Routes>
    <Route path='about' element={<About />} />
    <Route path='/' element={<Home />} />
