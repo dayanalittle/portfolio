8cef4b959bc1881e4a107be4963e1ba278c8c4fb
Day Little <dayana.y.little@gmail.com>
2023-02-02 22:23:48 -0500

    add project images from imgur

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,12 @@
 function Projects() {
     return (
+
+        <>
         <h1>Projects</h1>
+        <p><img src="https://i.imgur.com/X9m3cpa.png" alt="daythebarber" className="projectpics" width="600" /></p>
+        <p><img src="https://i.imgur.com/k7hKaRO.png" alt="connectfour" className="projectpics" width="600" /></p>
+
+        </>
     )
 }
 
