65015d7adb051a671e7839208fe607f34acc17cb
Day Little <dayana.y.little@gmail.com>
2023-02-03 00:05:32 -0500

    store project data

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,9 +3,6 @@ function Projects() {
 
         <>
         <h1>Projects</h1>
-        <p><img src="https://i.imgur.com/X9m3cpa.png" alt="daythebarber" className="projectpics" width="600" /></p>
-        <p><img src="https://i.imgur.com/k7hKaRO.png" alt="connectfour" className="projectpics" width="600" /></p>
-
         </>
     )
 }
