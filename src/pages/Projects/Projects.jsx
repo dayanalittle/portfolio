a0d971912422c75bbc2a474bf5f210138567f990
Day Little <dayana.y.little@gmail.com>
2023-02-03 00:17:15 -0500

    display a list of of all projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,19 @@
+import projects from "../../data/projects"
+
 function Projects() {
     return (
 
         <>
         <h1>Projects</h1>
+        <section>
+            {projects.map((project, idx) =>
+            <ul>
+            <li key={idx}>
+                {project.title}
+             </li> 
+             </ul>
+            )}
+        </section>
         </>
     )
 }
