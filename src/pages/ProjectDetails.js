d523ea25249936cf940bfeae0511c88fd920e011
Day Little <dayana.y.little@gmail.com>
2023-02-10 10:28:48 -0500

    fix project details

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -2,12 +2,13 @@ import { useParams } from "react-router-dom"
 import findProject from "../Utilities/findProjects"
 
 function ProjectDetails() {
-  const {ProjectDetails} = useParams()
-  const project = findProject(ProjectDetails)
+  const {projectDetails} = useParams()
+  const project = findProject(projectDetails)
   return (
     <>
       <h1>{project.title}</h1>
       <p>{project.description}</p>
+      <img src={project.image} alt={project.title} />
       <button>GitHub</button>
       <button>Deployed Site</button>
     </>
