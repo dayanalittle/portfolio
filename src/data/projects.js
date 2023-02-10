5fdcba5fa34b475cc9e35e374df54b688eccac22
Day Little <dayana.y.little@gmail.com>
2023-02-10 10:49:58 -0500

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,6 @@
 const Projects = [
   {
     title: "DayTheBarber",
-    image: <img src="https://i.imgur.com/X9m3cpa.png" alt="daythebarber" className="projectpics" width="600" />,
     description: "For years, I couldn't find a booking application thathad all the features I needed to successfully service all of my clients, so I created one! With this app my clients can create a profile by simply signing in using Google Auth. From there you can book, edit, reschedule, view, and leave reviews for appointments.",
     repositoryLink:"https://github.com/dayanalittle/Booking-app.git",
     deploymentLink:"https://bookdaythebarber.fly.dev/"
@@ -9,7 +8,6 @@ const Projects = [
 
   {
     title: "Connect Four",
-    image:<img src="https://i.imgur.com/k7hKaRO.png" alt="connectfour" className="projectpics" width="600" />,
     description:"Connect Four is a browser based game that accepts two players as opponents. In the game, players take turns choosing an index on the game board to place their chip, starting from the bottom row. Once a column is chosen, the chip will slot into the lowest spaces in each column.",
     repositoryLink:"https://github.com/dayanalittle/Connect-Four1.git",
     deploymentLink:"https://dlittleconnectfour.netlify.app/"
